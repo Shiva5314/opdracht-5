80395b27cf78410eee88ef39681a7ee3b64212b3
Shiva5314 <9018539@student.zadine.nl>
2022-10-13 13:13:34 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,14 @@
+alert("haalloooooo");
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+
+var kleur = prompt("geef een Engelse kleurnaam: (purple of green", "");
+
+// kleur aanpassen
+//document.getElementById("titel").style.color = "green";
+document.getElementById("kop").style.color = kleur;
+
+// tekst aanpassen
+document.getElementById("p1").style.color = "green";
